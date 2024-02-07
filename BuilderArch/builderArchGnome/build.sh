866ba7f6641d90dfee36566aec485fe2c0722247
Saveliy Burkov <saveliyburk@gmail.com>
2024-02-07 22:36:03 +0400

    Add instructions and add config in main folder

--- BuilderArch/builderArchGnome/build.sh
+++ BuilderArch/builderArchGnome/build.sh
@@ -2,9 +2,14 @@
 
 
 
-parentDir=$(cd $(dirname "$0"); pwd)
 
-rm -R $HOME/inst/
+echo '========Start Install========'
+
+parentDir=$(cd $(dirname "$0"); pwd)
+echo '========Copy Files========'
+if [ -d $HOME/inst/ ]; 
+then rm -R $HOME/inst/
+fi 
 
 mkdir $HOME/inst/
 mkdir $HOME/inst/listpkg/
@@ -18,9 +23,31 @@ sudo cp gen-otherlistpkg.hook /usr/share/libalpm/hooks/gen-otherlistpkg.hook
 cp gen-pacmanlistpkg.hook $HOME/inst/hooks/gen-pacmanlistpkg.hook
 sudo cp gen-pacmanlistpkg.hook /usr/share/libalpm/hooks/gen-pacmanlistpkg.hook
 
-#pacman -S --needed - < $HOME/inst/listpkg/pacmanpkg-list.txt
+echo '=======Install packages==========='
+
+#pacman -S --needed - < $HOME/inst/listpkg/pacmanpkg-list.txt 
+
+echo '==========Setup Locale Settings=========='
+
+# echo 'en_US.UTF8' > /etc/locale.gen
+# echo 'ru_RU.UTF8' > /etc/locale.gen
+# locale-gen
+# ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+
+echo '==============Copy Config Files============'
+
+# if [ -d $HOME/.icons/ ]; 
+# then rm -R $HOME/.icons/
+# fi 
+
+# mkdir $HOME/.icons/
+# cp $parentDir/../icons/* $HOME/.icons/*
 
 
-
+# if [ -d $HOME/.config/ ]; then 
+# cp $parentDir/../config/* $HOME/.config/*
+# else mkdir $HOME/.config/
+# cp $parentDir/../config/* $HOME/.config/*
+# fi
 
 echo 'Final!'
